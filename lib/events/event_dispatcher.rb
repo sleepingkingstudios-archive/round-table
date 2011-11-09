03d18221ac3ed6a07e64fe92fa70c3dae15c9282
Rob Smith <sleepingkingstudios@gmail.com>
2011-11-09 13:03:53 -0500

    Implemented event bubbling
    To enable event bubbling, define a @parent ivar, or set the __bubble_attr__ to a
    valid string or symbol. Valid symbols include :@@foo to use a cvar, :@bar to use
    an ivar, or :baz to use an instance method. Remember to set event[:bubbles] =
    true, or dispatch_event event, :bubbles => :true.

--- lib/events/event_dispatcher.rb
+++ lib/events/event_dispatcher.rb
@@ -22,6 +22,8 @@ module RoundTable::Events
           callback.call event
         end # each
       end # if
+      
+      bubble_event(event) if event[:bubbles]
     end # method dispatch_event
     
     #################
@@ -79,5 +81,39 @@ module RoundTable::Events
     def remove_generic_listener(callback)
       self.remove_listener :*, callback
     end # method remove_generic_listener
+    
+    ################
+    # Event Bubbling
+    
+    def __bubble_attr__
+      @__bubble_attr__ || :@parent
+    end # method __bubble_attr__
+    
+    def __bubble_attr__=(attribute)
+      @__bubble_attr__ = attribute if attribute.is_a?(String) || attribute.is_a?(Symbol)
+    end # method __bubble_attr__
+    private :__bubble_attr__, :__bubble_attr__=
+    
+    def bubble_parent
+      if __bubble_attr__.to_s =~ /^\@\@/
+        # class variable
+        self.class.class_eval __bubble_attr__.to_s.split(/\s+/).first
+        
+        # The .split().first code is an attempt to protect against malicious
+        # code, e.g. __bubble_attr__ = "@@foo; do_something_evil"
+      elsif __bubble_attr__.to_s =~ /^\@/
+        # instance variable
+        self.instance_variable_get __bubble_attr__
+      else
+        # instance method
+        self.send __bubble_attr__
+      end # if-elsif-else
+    end # method bubble_parent
+    
+    def bubble_event(event)
+      return unless self.bubble_parent.is_a? EventDispatcher
+      
+      self.bubble_parent.dispatch_event(event)
+    end # method bubble_event
   end # module EventDispatcher
 end # module RoundTable::Events
--- spec/events/event_dispatcher_spec.rb
+++ spec/events/event_dispatcher_spec.rb
@@ -10,6 +10,20 @@ module RoundTable::Mock
     class MockEventDispatcher
       include RoundTable::Events::EventDispatcher
     end # class MockEventDispatcher
+    
+    class MockEventDispatcherWithBubbling < MockEventDispatcher
+      @@foo = :bar
+      
+      def initialize
+        @baz = :bizzle
+      end # method initialize
+      
+      def wibble
+        :wobble
+      end # method wibble
+      
+      attr_accessor :parent
+    end # class MockEventDispatcherWithBubbling
   end # module Events
 end # module RoundTable::Mock
 
@@ -61,19 +75,6 @@ describe RoundTable::Events::EventDispatcher do
     subject.dispatch_event @event
   end # it triggers callbacks on dispatched events
   
-  it "can add generic listeners which trigger on all events" do
-    other_callback = double('callable')
-    other_callback.stub(:call)
-    
-    subject.add_listener :*, @callback
-    subject.add_generic_listener other_callback
-    
-    @callback.should_receive(:call).exactly(2)
-    other_callback.should_receive(:call).exactly(2)
-    subject.dispatch_event @event
-    subject.dispatch_event Event.new :other_type
-  end # it can add generic listeners which trigger on all events
-  
   it "can remove event listeners" do
     expect { subject.remove_listener }.to raise_error ArgumentError
     expect { subject.remove_listener :event_type }.to raise_error ArgumentError
@@ -107,6 +108,22 @@ describe RoundTable::Events::EventDispatcher do
     subject.dispatch_event Event.new :other_type
   end # it can remove ... matching a block
   
+  #########################
+  # Generic Event Listeners
+  
+  it "can add generic listeners which trigger on all events" do
+    other_callback = double('callable')
+    other_callback.stub(:call)
+    
+    subject.add_listener :*, @callback
+    subject.add_generic_listener other_callback
+    
+    @callback.should_receive(:call).exactly(2)
+    other_callback.should_receive(:call).exactly(2)
+    subject.dispatch_event @event
+    subject.dispatch_event Event.new :other_type
+  end # it can add generic listeners which trigger on all events
+  
   it "can remove generic event listeners" do
     other_callback = double('mock_callable')
     other_callback.stub(:call)
@@ -120,4 +137,57 @@ describe RoundTable::Events::EventDispatcher do
     other_callback.should_receive(:call)
     subject.dispatch_event @event
   end # it can remove generic event listeners
+  
+  ################
+  # Event Bubbling
+  
+  it "can set a bubble parent" do
+    parent = MockEventDispatcher.new
+    child = MockEventDispatcherWithBubbling.new
+    
+    child.bubble_parent.should be nil
+    
+    child.instance_eval {
+      self.send :__bubble_attr__=, :@@foo
+    } # end instance_eval
+    child.bubble_parent.should be :bar
+    
+    child.instance_eval {
+      self.send :__bubble_attr__=, :@baz
+    } # end instance_eval
+    child.bubble_parent.should be :bizzle
+    
+    child.instance_eval {
+      self.send :__bubble_attr__=, :wibble
+    } # end instance_eval
+    child.bubble_parent.should be :wobble
+  end # it can set a bubble parent
+  
+  it "can bubble events" do
+    parent = MockEventDispatcher.new
+    child = MockEventDispatcherWithBubbling.new
+    child.parent = parent
+    child.bubble_parent.should be parent
+    
+    callback_has_been_called = false
+    callback = Proc.new do |event|
+      event.should be @event
+      callback_has_been_called = true
+      event[:original_dispatcher].should be child
+      event[:current_dispatcher].should be parent
+    end # proc callback
+    
+    parent.add_listener :event_type, callback
+    @event[:bubbles] = true
+    child.dispatch_event @event
+    callback_has_been_called.should be true
+    
+    parent.add_listener :other_type, @callback
+    @callback.should_receive(:call).once
+    child.dispatch_event Event.new :other_type, :bubbles => :true
+    
+    parent.add_listener :third_type, @callback
+    @callback.should_not_receive(:call)
+    child.dispatch_event Event.new :third_type
+  end # it can bubble events
 end # describe EventDispatcher
